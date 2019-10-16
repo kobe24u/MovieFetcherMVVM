23fb9f391462e67e04b014d29d452dad1464b710
kobe24u <kobe24u@vip.qq.com>
2019-10-16 20:20:45 +1100

    Initial Commit

---
+++ MovieFetcherMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieFetcherMVVM
+//
+//  Created by Vinnie Liu on 16/10/19.
+//  Copyright © 2019 Yawei Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
